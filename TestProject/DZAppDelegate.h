1f888705c59ec3d912c33a9ac7e482cc742c333b
msk <78728930@qq.com>
2013-10-23 09:49:42 +0800

    Initial Commit

---
+++ TestProject/DZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DZAppDelegate.h
+//  TestProject
+//
+//  Created by msk on 13-10-23.
+//  Copyright (c) 2013年 AHDZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TestProject/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TestProject
+//
+//  Created by msk on 13-10-23.
+//  Copyright (c) 2013年 AHDZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DZAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([DZAppDelegate class]));
+	}
+}
